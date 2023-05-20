3d9c06a520c7b7e6fdeafc2bae3f013cae73ff97
JVelezFD <jvelez117@gmail.com>
2023-05-20 13:27:21 -0400

    adding initial code to change to class

--- budget.py
+++ budget.py
@@ -1,45 +1,48 @@
-available = 2500.00
-budgets = {}
-expenditure = {}
 
-def add_budget(name, amount):
-    global available
-    
-    if name in budgets:
-        raise ValueError ("Budget exists")
-    
-    if amount > available:
-        raise ValueError("Insufficient funds")
-    
-    budgets[name] = amount
-    available -= amount
-    expenditure[name] = 0 
-    return available
+class BudgetManager:
+    def __init__(self, amount):
+        self.available = amount
+        self.budgets = {}
+        self.expenditure = {}
 
-def spend(name, amount):
-    if name not in expenditure:
-        raise ValueError("No such budget")
-    expenditure[name] += amount
-    budgeted   = budgets[name]
-    spent = expenditure[name]
-    return budgeted - spent
-
-def print_summary():
-    print("Budget           Budgeted       Spent    Remaining")
-    print("--------------- ---------- ---------- ----------") 
-    total_budgeted =0 
-    total_spent = 0
-    total_remaining = 0
-    for name in budgets:
-        budgeted = budgets[name]
-        spent = expenditure[name]
-        remaining = budgeted - spent
-        print(f'{name:15s} {budgeted:10.2f} {spent:10.2f}' 
-              f'{remaining:10.2f}')
-        total_budgeted += budgeted
-        total_spent += spent
-        total_remaining += remaining
-    print("--------------- ---------- ---------- ----------") 
-    print(f'{"Total":15s} {total_budgeted:10.2f} {total_spent:10.2f}' 
-              f'{total_budgeted - total_spent:10.2f}')
+    def add_budget(name, amount):
+        global available
         
+        if name in budgets:
+            raise ValueError ("Budget exists")
+        
+        if amount > available:
+            raise ValueError("Insufficient funds")
+        
+        budgets[name] = amount
+        available -= amount
+        expenditure[name] = 0 
+        return available
+
+    def spend(name, amount):
+        if name not in expenditure:
+            raise ValueError("No such budget")
+        expenditure[name] += amount
+        budgeted   = budgets[name]
+        spent = expenditure[name]
+        return budgeted - spent
+
+    def print_summary():
+        print("Budget           Budgeted       Spent    Remaining")
+        print("--------------- ---------- ---------- ----------") 
+        total_budgeted =0 
+        total_spent = 0
+        total_remaining = 0
+        for name in budgets:
+            budgeted = budgets[name]
+            spent = expenditure[name]
+            remaining = budgeted - spent
+            print(f'{name:15s} {budgeted:10.2f} {spent:10.2f}' 
+                f'{remaining:10.2f}')
+            total_budgeted += budgeted
+            total_spent += spent
+            total_remaining += remaining
+        print("--------------- ---------- ---------- ----------") 
+        print(f'{"Total":15s} {total_budgeted:10.2f} {total_spent:10.2f}' 
+                f'{total_budgeted - total_spent:10.2f}')
+            
